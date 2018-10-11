0004c38acac8e62708892fecc7bfc8fb88b255c6
takami00880044 <takami@key-p.co.jp>
2018-10-11 13:50:34 +0900

    test: テスト更新

---
+++ easy-Tester/test/circleci_test.testsuites.py
@@ -0,0 +1,26 @@
+#coding:utf-8
+import sys
+import os
+
+TEST_PATH = os.path.dirname(os.path.abspath(__file__)) + '/'
+BASE_PATH = TEST_PATH + '../'
+TOOL_PATH = BASE_PATH + 'tool/'
+
+sys.path.append(TOOL_PATH + 'core/')
+from tester import Tester
+
+tester = Tester()
+
+tester.setTestSuites('CircleCI Test')
+
+tester.addTestSuite('test')
+
+tester.addTestCase('テスト',[
+    [TEST_PATH + 'testcase/test.testcase.yml']
+])
+
+tester.execute(
+    browser='Chrome',
+    url='http://localhost:10001',
+    wait_seconds=3
+)
