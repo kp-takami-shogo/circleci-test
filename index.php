e300219bac32612989f8d9aa0659e49423030fe2
takami00880044 <takami@key-p.co.jp>
2019-01-09 11:15:09 +0900

    refactor: Change index.php

--- index.php
+++ index.php
@@ -3,9 +3,12 @@
 
 <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.css">
+  <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
 
   <link rel="stylesheet" href="style.css">
-
   <script src="main.js"></script>
 
   <title>CircleCI デモ</title>
@@ -14,9 +17,96 @@
 
 <body>
 
-<span id="test">Test</span>
+<div class="container">
 
-<input type="text" id="test-input">
+  <h1 class="title">CircleCI デモ</h1>
+
+  <div class="field name-field">
+    <label class="label">Name</label>
+    <div class="control">
+      <input class="input" type="text" placeholder="Text input" value="CircleCI">
+    </div>
+  </div>
+
+  <div class="field username-field">
+    <label class="label">Username</label>
+    <div class="control has-icons-left has-icons-right">
+      <input class="input is-success" type="text" placeholder="Text input" value="circleci">
+      <span class="icon is-small is-left">
+      <i class="fas fa-user"></i>
+    </span>
+      <span class="icon is-small is-right">
+      <i class="fas fa-check"></i>
+    </span>
+    </div>
+    <p class="help is-success">This username is available</p>
+  </div>
+
+  <div class="field email-field">
+    <label class="label">Email</label>
+    <div class="control has-icons-left has-icons-right">
+      <input class="input is-danger" type="email" placeholder="Email input" value="circleci@">
+      <span class="icon is-small is-left">
+      <i class="fas fa-envelope"></i>
+    </span>
+      <span class="icon is-small is-right">
+      <i class="fas fa-exclamation-triangle"></i>
+    </span>
+    </div>
+    <p class="help is-danger">This email is invalid</p>
+  </div>
+
+  <div class="field subject-field">
+    <label class="label">Subject</label>
+    <div class="control">
+      <div class="select">
+        <select>
+          <option value="CircleCI" selected>CircleCI</option>
+          <option value="デモ">デモ</option>
+        </select>
+      </div>
+    </div>
+  </div>
+
+  <div class="field message-field">
+    <label class="label">Message</label>
+    <div class="control">
+      <textarea class="textarea" placeholder="Textarea">CircleCI デモ</textarea>
+    </div>
+  </div>
+
+  <div class="field terms-conditions-field">
+    <div class="control">
+      <label class="checkbox">
+        <input type="checkbox" checked>
+        I agree to the <a href="#">terms and conditions</a>
+      </label>
+    </div>
+  </div>
+
+  <div class="field question-field">
+    <div class="control">
+      <label class="radio">
+        <input type="radio" name="question" checked>
+        Yes
+      </label>
+      <label class="radio">
+        <input type="radio" name="question">
+        No
+      </label>
+    </div>
+  </div>
+
+  <div class="field is-grouped">
+    <div class="control">
+      <button class="button is-link">Submit</button>
+    </div>
+    <div class="control">
+      <button class="button is-text">Cancel</button>
+    </div>
+  </div>
+
+</div>
 
 </body>
 
